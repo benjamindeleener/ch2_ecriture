ce573879030e2f5d875045822e3d7df981c16eb0
Benjamin De Leener <benjamin.de-leener@polymtl.ca>
2020-09-09 07:25:02 -0400

    Update test_exercice.py

--- test_exercice.py
+++ test_exercice.py
@@ -9,59 +9,16 @@ import exercice
 
 class TestExercice(unittest.TestCase):
     def setUp(self):
-        with open('./data/sample_countries.txt') as f:
-            self.countries = [name.replace('\n', '') for name in f.readlines()]
-
-    def test_capitalize_first_letter(self):
-        names = [
-            'Angola',
-            'The Bahamas',
-            'Andorra',
-            'Antigua and Barbuda',
-            'Georgia'
-        ]
-        altered_names = [x[0].lower() + x[1:] for x in names]
-        output = list(map(exercice.capitaliser_pays, altered_names))
-        output_first_letters = list(map(lambda name: name[0], output))
-        expected_first_letters = list(map(lambda name: name[0], names))
-        self.assertListEqual(
-            output_first_letters,
-            expected_first_letters,
-            'Mettre la permiere lettre en majuscule'
-        )
-
-    def test_lower_case_names(self):
-        altered_names = [country.lower() for country in self.countries]
-        output = list(map(exercice.capitaliser_pays, altered_names))
-        self.assertListEqual(
-            output,
-            self.countries,
-            'Certaines lettres ne doivent pas etre en minuscules'
-        )
-
+        with open('./data/sample_words.txt') as f:
+            self.words = [word.replace('\n', '') for word in f.readlines()]
+    
     def test_upper_case_names(self):
-        altered_names = [country.upper() for country in self.countries]
-        output = list(map(exercice.capitaliser_pays, altered_names))
+        altered_names = [word.upper() for word in self.countries]
+        output = list(map(exercice.majuscule, altered_names))
         self.assertListEqual(
             output,
-            self.countries,
-            'Certaines lettres ne doivent pas etre en majuscule'
-        )
-
-    def test_linking_words(self):
-        names = [
-            'Antigua and Barbuda',
-            'Bosnia and Herzegovina'
-        ]
-        altered_names = [
-            'Antigua aNd Barbuda',
-            'Bosnia ANd Herzegovina'
-        ]
-        output = list(map(exercice.capitaliser_pays, altered_names))
-        self.assertListEqual(
-            output,
-            names,
-            'Les mots de liaison doivent etre en minuscule'
+            self.words,
+            'Toutes les lettres doivent être en majuscule.'
         )
 
 
